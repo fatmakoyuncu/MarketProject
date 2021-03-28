df13e12d96ca554f0427e23ac0d554caed47788d
fatma koyuncu <fatmakyc16@gmail.com>
2021-03-28 22:26:15 +0300

    initial commit

--- Backend/app.js
+++ Backend/app.js
@@ -35,18 +35,27 @@ app.get('/image/:id', async (req, res) => {
     }
     const product_info = products[0];
     console.log(product_info);
-
-    // fs.readFile(product_info.img_path, (err, data) => {
-    //     if (err) {
-    //         throw err
-    //     }
-    //     res.writeHead(200, { 'Content-Type': 'image/jpg' })
-    //     res.end(data);
-    // })
     res.set('Content-Type', 'image/jpg')
     fs.createReadStream(product_info.img_path).pipe(res);
+})
 
+const category = async (id) => {
+    const result = await client.query(`SELECT category_name, category_icon FROM categories WHERE id = ${id}`)
+    return result.rows
+}
 
+app.get('/categories/icon/:id' , async (req, res) => {
+    const category_id = req.params.id
+    const categories = await category(category_id);
+
+    if(categories.length !== 1){
+        return res.json({success: false, message: 'category not found'})
+    }
+
+    const category_info = categories[0];
+    console.log(category_info);
+    res.set('Content-Type', 'image/png')
+    fs.createReadStream(category_info.category_icon).pipe(res);
 })
 
 
